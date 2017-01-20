9902a010eb2365445c452c448f55240954df5773
Mitsuaki Takeuchi <m2takeuchi@gmail.com>
2017-01-20 15:14:36 +0900

    ignore .cask/

--- .emacs.d/.cask/24.3/bootstrap/git-20140128.241/git-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; git-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("git-pkg.el" "git.el") (22624 42613 316785
-;;;;;;  484000))
-
-;;;***
-
-(provide 'git-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; git-autoloads.el ends here
